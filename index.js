324f4cc1fbd2acabe217777e1405573b95d057cb
liang4793 <3500506060@qq.com>
2022-05-04 10:34:13 +0800

    notice_box close

---
+++ index.js
@@ -0,0 +1,5 @@
+var notice_box = document.querySelector(".notice_box");
+var notice_box_button = document.querySelectorAll(".notice_box_button");
+notice_box_button[1].addEventListener("click", function () {
+    notice_box.style.display = "none";
+});
