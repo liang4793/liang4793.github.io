be464b971b9271af4a3bb08ec3c1ace3e41bc476
liang4793 <3500506060@qq.com>
2022-05-23 17:27:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let notice_box = document.querySelector(".notice_box");
 let notice_box_button = document.querySelectorAll(".notice_box_button");
 
 notice_box.style.display = "block";
-notice_box.style.animation = "notice_box_pop 0.15s linear forwards";
+notice_box.style.animation = "notice_box_pop 0.4s cubic-bezier(0.22, 0.61, 0.36, 1) 0s 1 normal forwards";
 
 notice_box_button[1].addEventListener("click", () => {
     notice_box.style.display = "none";
