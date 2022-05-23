a8e4fa6a038abe454a78f5cbd58b31442434ceb1
liang4793 <3500506060@qq.com>
2022-05-23 16:12:56 +0800

    notice_box pop animation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let notice_box = document.querySelector(".notice_box");
 let notice_box_button = document.querySelectorAll(".notice_box_button");
+
+notice_box.style.display = "block";
+notice_box.style.animation = "notice_box_pop 0.15s linear forwards";
+
 notice_box_button[1].addEventListener("click", () => {
     notice_box.style.display = "none";
 });
