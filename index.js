6ff662edb28dae7a9e46187e2a8a1ecaa5004daf
liang4793 <3500506060@qq.com>
2022-05-30 13:41:28 +0800

    add text_icon

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /* notice */
+let show_notice = "True";
 let notice_box = document.querySelector(".notice_box");
 let notice_box_button = document.querySelectorAll(".notice_box_button");
 
-notice_box.style.display = "block";
-notice_box.style.animation = "notice_box_pop 0.4s cubic-bezier(0.22, 0.61, 0.36, 1) 0s 1 normal forwards";
+if (show_notice == "True") {
+    notice_box.style.display = "block";
+    notice_box.style.animation = "notice_box_pop 0.4s cubic-bezier(0.22, 0.61, 0.36, 1) 0s 1 normal forwards";
 
-notice_box_button[1].addEventListener("click", () => {
-    notice_box.style.display = "none";
-});
+    notice_box_button[1].addEventListener("click", () => {
+        notice_box.style.display = "none";
+    });
+}
+
+/* page */
