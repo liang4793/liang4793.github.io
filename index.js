fc7c482b51da168b28e261b6a8981ec9aedc2987
liang4793 <3500506060@qq.com>
2022-07-15 11:56:16 +0800

    update

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 console.log("Welcome to Liang4793's Repository!");
 
 /* notice */
-let show_notice = "True";
-let notice_box = document.querySelector(".notice_box");
-let notice_box_button = document.querySelectorAll(".notice_box_button");
+let showNotice = "True";
+let noticeBox = document.querySelector(".notice_box");
+let noticeBoxButton = document.querySelectorAll(".notice_box_button");
 
-console.log('show_notice',show_notice);
+console.log('showNotice =>',showNotice);
 
-if (show_notice == "True") {
-    notice_box.style.display = "block";
-    notice_box.style.animation = "notice_box_pop 0.4s cubic-bezier(0.22, 0.61, 0.36, 1) 0s 1 normal forwards";
+if (showNotice == "True") {
+    noticeBox.style.display = "block";
+    noticeBox.style.animation = "notice_box_pop 0.4s cubic-bezier(0.22, 0.61, 0.36, 1) 0s 1 normal forwards";
 
-    notice_box_button[1].addEventListener("click", () => {
-        notice_box.style.display = "none";
+    noticeBoxButton[1].addEventListener("click", () => {
+        noticeBox.style.display = "none";
         console.log("notice closed by user")
     });
 }
