497cd868d88455e22f16c1dc1c1e54add7ecddfe
Liáng4793 <3500506060@qq.com>
2024-03-03 13:47:54 +0800

    update

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ function startPage() {
 //main
 let shade2 = document.getElementById("shade2");
 let quit = document.getElementById("quit");
+let l = document.getElementById("location");
+let me = document.getElementById("me");
+let repo = document.getElementById("repo");
+
+//locationBox
+let lBox = document.getElementById("locationBox");
+let map = document.getElementById("map");
+let LL = document.getElementById("LL");
+let SS = document.getElementById("SS");
+let timeTitle = document.getElementById("timeTitle");
+let localTime = document.getElementById("localTime");
+
+let meBox = document.getElementById("meBox");
+let repoBox = document.getElementById("repoBox");
 
 quit.addEventListener("click", () => {
     shade2.style.animation = "disappear 0.2s ease-in forwards";
@@ -36,20 +50,48 @@ quit.addEventListener("click", () => {
     setTimeout(() => {
         shade2.style.display = "none";
         container.style.display = "block";
+
+        lBox.style.display = "none";
+        map.style.display = "none";
+        LL.style.display = "none";
+        SS.style.display = "none";
+        timeTitle.style.display = "none";
+
+        meBox.style.display = "none";
+        repoBox.style.display = "none";
     }, 200);
 });
 
-let l = document.getElementById("location");
-let me = document.getElementById("me");
-let repo = document.getElementById("repo");
-
+function updateLocalTime() {
+    var time = new Date().toLocaleString("en-US", {timeZone: "Asia/Shanghai"});
+    localTime.innerHTML = time;
+}
 l.addEventListener("click", () => {
+    updateLocalTime()
+    setInterval(updateLocalTime, 1000);
     shade2.style.animation = "appear 0.2s ease-in forwards";
     container.style.animation = "disappear 0.2s ease-in forwards";
     setTimeout(() => {
         shade2.style.display = "block";
         container.style.display = "none";
     }, 200);
+    lBox.style.display = "block";
+    map.style.animation = "fromL 0.4s ease forwards";
+    setTimeout(() => {
+        map.style.display = "block";
+    }, 400);
+    LL.style.animation = "fromT 0.4s ease forwards";
+    setTimeout(() => {
+        LL.style.display = "block";
+    }, 400);
+    SS.style.animation = "fromT 0.4s ease forwards";
+    setTimeout(() => {
+        SS.style.display = "block";
+    }, 400);
+    timeTitle.style.animation = "fromT 0.4s ease forwards";
+    setTimeout(() => {
+        timeTitle.style.display = "block";
+    }, 400);
 });
 
 me.addEventListener("click", () => {
