2a2f020142725c1b823b5feb499dac9eb6c12c0b
Liáng4793 <3500506060@qq.com>
2024-05-11 20:45:01 +0800

    update

--- index.js
+++ index.js
@@ -41,7 +41,17 @@ let SS = document.getElementById("SS");
 let timeTitle = document.getElementById("timeTitle");
 let localTime = document.getElementById("localTime");
 
+//meBox
 let meBox = document.getElementById("meBox");
+let mePic = document.getElementsByClassName("mePicBox2");
+let toLeft = document.getElementById("toLeft");
+let toRight = document.getElementById("toRight");
+let label1 = document.getElementById("label1");
+let label2 = document.getElementById("label2");
+let contactTitle = document.getElementById("contactTitle");
+let contactBox = document.getElementById("contactBox");
+
+//repoBox
 let repoBox = document.getElementById("repoBox");
 
 quit.addEventListener("click", () => {
@@ -58,16 +68,24 @@ quit.addEventListener("click", () => {
         timeTitle.style.display = "none";
 
         meBox.style.display = "none";
+        toLeft.style.display = "none";
+        toRight.style.display = "none";
+        label1.style.display = "none";
+        label2.style.display = "none";
+        contactTitle.style.display = "none";
+        contactBox.style.display = "none";
+
         repoBox.style.display = "none";
     }, 200);
 });
 
+//locationBox
 function updateLocalTime() {
     var time = new Date().toLocaleString("en-US", {timeZone: "Asia/Shanghai"});
     localTime.innerHTML = time;
-}
+};
 l.addEventListener("click", () => {
-    updateLocalTime()
+    updateLocalTime();
     setInterval(updateLocalTime, 1000);
     shade2.style.animation = "appear 0.2s ease-in forwards";
     container.style.animation = "disappear 0.2s ease-in forwards";
@@ -76,7 +94,7 @@ l.addEventListener("click", () => {
         container.style.display = "none";
     }, 200);
     lBox.style.display = "block";
-    map.style.animation = "fromL 0.4s ease forwards";
+    map.style.animation = "fromL2 0.4s ease forwards";
     setTimeout(() => {
         map.style.display = "block";
     }, 400);
@@ -94,6 +112,34 @@ l.addEventListener("click", () => {
     }, 400);
 });
 
+//meBox
+var mePicList = [1, 2, 3, 4];
+toLeft.addEventListener("click", () => {
+    mePicList.unshift(mePicList.pop());
+    mePic[mePicList[0]-1].style.animation = "to4 reverse 1.2s ease forwards";
+    setTimeout(() => {
+        mePic[mePicList[3]-1].style.animation = "to3 reverse 0.4s ease forwards";
+        setTimeout(() => {
+            mePic[mePicList[2]-1].style.animation = "to2 reverse 0.4s ease forwards";
+            setTimeout(() => {
+                mePic[mePicList[1]-1].style.animation = "to1 reverse 0.4s ease forwards";
+            },200);
+        },200);
+    },200);
+});
+toRight.addEventListener("click", () => {
+    mePicList.push(mePicList.shift());
+    mePic[mePicList[3]-1].style.animation = "to4 1.2s ease forwards";
+    setTimeout(() => {
+        mePic[mePicList[0]-1].style.animation = "to1 0.4s ease forwards";
+        setTimeout(() => {
+            mePic[mePicList[1]-1].style.animation = "to2 0.4s ease forwards";
+            setTimeout(() => {
+                mePic[mePicList[2]-1].style.animation = "to3 0.4s ease forwards";
+            },200);
+        },200);
+    },200);
+});
 me.addEventListener("click", () => {
     shade2.style.animation = "appear 0.2s ease-in forwards";
     container.style.animation = "disappear 0.2s ease-in forwards";
@@ -101,8 +147,30 @@ me.addEventListener("click", () => {
         shade2.style.display = "block";
         container.style.display = "none";
     }, 200);
+    meBox.style.display = "block";
+    toLeft.style.animation = "fromR 0.4s ease forwards";
+    setTimeout(() => {
+        toLeft.style.display = "block";
+    }, 400);
+    toRight.style.animation = "fromL 0.4s ease forwards";
+    setTimeout(() => {
+        toRight.style.display = "block";
+    }, 400);
+    label1.style.animation = "appear 0.4s ease forwards";
+    label2.style.animation = "appear 0.4s ease forwards";
+    setTimeout(() => {
+        label1.style.display = "block";
+        label2.style.display = "block";
+    }, 400);
+    contactTitle.style.animation = "fromT 0.4s ease forwards";
+    contactBox.style.animation = "fromT 0.4s ease forwards";
+    setTimeout(() => {
+        contactTitle.style.display = "block";
+        contactBox.style.display = "block";
+    }, 400);
 });
 
+//repoBox
 repo.addEventListener("click", () => {
     shade2.style.animation = "appear 0.2s ease-in forwards";
     container.style.animation = "disappear 0.2s ease-in forwards";
@@ -112,6 +180,7 @@ repo.addEventListener("click", () => {
     }, 200);
 });
 
+//main
 let s = document.getElementById("scroll");
 let projects = document.getElementById("projects");
 
